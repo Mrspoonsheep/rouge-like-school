7798da1eda7b34ce01964886895906e7b1c0fc21
Mrspoonsheep <34350498+Mrspoonsheep@users.noreply.github.com>
2021-09-14 22:09:17 +0200

    partway through first room argument
    almost finished a prototype version of the first-room-spawn argument

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,58 @@ using UnityEngine;
 public class Generator : MonoBehaviour
 {
     private System.Random rand = new System.Random();
-    private Transform spawnerTransform;
+    private Transform spawnerTransform, roomTransform;
     public GameObject spawner;
-    private float xpos, ypos;
+    private float xpos, ypos, preNumber;
+    int gridMiddle;
     void Start()
     {
         spawnerTransform = spawner.transform;
         int gridVariationX = rand.Next(99, 199);
         int gridVariationY = rand.Next(59, 99);
         Grid generationGrid = new Grid(gridVariationX, gridVariationY, 32f, spawnerTransform);
-        
+        preNumber = (gridVariationX / 2) - 0.5f;
+        string strNumber = strNumber = preNumber.ToString();
+
+        if(!(gridVariationX%2 == 0))
+        {
+        try
+        {
+            Debug.Log($"strNumebr = {strNumber}");
+            gridMiddle = int.Parse(strNumber);
+        }
+        catch (Exception c)
+        {
+            print(c);
+        }
+        }
         for(int i = 0; i < generationGrid.gridCells.Count; i++)
         {
-            if(generationGrid.gridCells[i].Position.x == generationGrid.gridCells[gridVariationX/2].Position.x)
+            if(generationGrid.gridCells[i].Position.x == generationGrid.gridCells[gridVariationX].Position.x)
             {
-
+                if (generationGrid.gridCells[i].Position.y == generationGrid.gridCells[4].Position.y)
+                {
+                    roomTransform.position = generationGrid.gridCells[i].Position;
+                    UnityEngine.Object room = GameObject.Find("Bfab");
+                    GameObject gameRoom = GameObject.Find("Bfab");
+                    Instantiate(room, generationGrid.gridCells[i].Position, gameRoom.transform.rotation);
+                }
             }
-            else if(generationGrid.gridCells[i].Position.x == generationGrid.gridCells[(gridVariationX/2) - gridVariationX % 2].Position.x)
+            else if(generationGrid.gridCells[i].Position.x == generationGrid.gridCells[gridMiddle].Position.x)
+            {
+                if (generationGrid.gridCells[i].Position.y == generationGrid.gridCells[4].Position.y)
+                {
+                    roomTransform.position = generationGrid.gridCells[i].Position;
+                    UnityEngine.Object room = GameObject.Find("Bfab");
+                    GameObject gameRoom = GameObject.Find("Bfab");
+                    Instantiate(room, generationGrid.gridCells[i].Position, gameRoom.transform.rotation);
+                }
+            }
+            else
             {
 
             }
         }
+
     }
 }
