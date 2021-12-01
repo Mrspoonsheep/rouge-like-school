9574eaba2abf667212b0e18d67484a0970ef1c22
Mrspoonsheep <34350498+Mrspoonsheep@users.noreply.github.com>
2021-12-01 14:33:28 +0100

    fixed the animations with help of hackerman
    veri cool

--- Assets/playercontrol.cs
+++ Assets/playercontrol.cs
@@ -15,7 +15,6 @@ public class playercontrol : MonoBehaviour
     public SpriteRenderer armSprite;
     public PointTowardMouse accessedScript;
     public Animator bodyAnim;
-
     private Vector2 FindGameObjPos(string strgameobj)
     {
         GameObject gameobj = GameObject.FindGameObjectWithTag(strgameobj);
@@ -25,15 +24,12 @@ public class playercontrol : MonoBehaviour
         Vector2 playerState = gameobj.transform.position;
         return playerState;
     }
-
     private void Start()
     {
-         // = GetComponentInChildren<Animator>();
+        // = GetComponentInChildren<Animator>();
     }
     Vector2 Teleport(Vector2 playerState, Vector2 dir)
     {
-
-
         playerState += dir;
         rb.MovePosition(playerState);
         // Debug.Log($"distanceX = {distanceX} and distanceY = {distanceY}");
@@ -44,6 +40,11 @@ public class playercontrol : MonoBehaviour
         Vector2 playerState = FindGameObjPos("Player");
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 move = new Vector2(moveX, moveY);
+
+        bodyAnim.SetBool("Walking", move.sqrMagnitude > 0.001);
+
         Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
         // if  (Input.GetKeyDown(KeyCode.LeftShift) && playerState.x - lastplace.x != 0 && playerState.y - lastplace.y != 0)
         if (Input.GetKeyDown(KeyCode.LeftShift) && rb.velocity.sqrMagnitude > 0.1)
@@ -54,7 +55,7 @@ public class playercontrol : MonoBehaviour
         //Debug.Log($"moveX = {moveX} and moveY = {moveY}");
         movement = new Vector2(moveX, moveY).normalized;
         lastplace = FindGameObjPos("Player");
-        
+
         bool shouldFlip = accessedScript.angle > 90f || accessedScript.angle < -90f;
 
         characterSprite.flipX = shouldFlip;
@@ -64,15 +65,23 @@ public class playercontrol : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(movement.x * movementSpeed, movement.y * movementSpeed);
-        if(movement.x != 0 || movement.y != 0)
-        {
-            bodyAnim.ResetTrigger("fWalkTrigger");
-            bodyAnim.SetTrigger("fWalkTrigger");
-        }
-        else
-        {
-            bodyAnim.ResetTrigger("stopWalkTrigger");
-            bodyAnim.SetTrigger("stopWalkTrigger");
-        }
+        //if (movement.x != 0 || movement.y != 0)
+        //{
+        //    Container.StartAnimPlayback(this, "fWalkTrigger");
+        //}
+        //else
+        //{
+        //    Container.StartAnimPlayback(this, "stopWalkTrigger");
+        //}
     }
+    public void StartAnimPlayback(playercontrol p, string animName)
+    {
+        //p.bodyAnim.ResetTrigger(animName);
+        p.bodyAnim.SetTrigger(animName);
+    }
+}
+
+public class Container
+{
+
 }
